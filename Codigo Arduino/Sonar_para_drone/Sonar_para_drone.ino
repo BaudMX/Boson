1b6c8240dab65aba994e7e7073f488555e8146bd
fedetarrats <32987207+fedetarrats@users.noreply.github.com>
2017-11-13 17:48:14 -0600

    Sonar última versión

--- Codigo Arduino/Sonar_para_drone/Sonar_para_drone.ino
+++ Codigo Arduino/Sonar_para_drone/Sonar_para_drone.ino
@@ -2,9 +2,6 @@
 #define ECHO_PIN_x 2
 #define PWM_PIN 5
 
-//#define TRIGGER_PIN_y 5
-//#define ECHO_PIN_y 4
-
 #define USONIC_DIV 58.0
 #define MEASURE_SAMPLE_DELAY 10
 #define MEASURE_SAMPLES 18
@@ -12,10 +9,8 @@
 
 
 String x = "";
-String y = "";
 
 char x_[50];
-char y_[50];
 
 void setup()
 {
@@ -28,15 +23,7 @@ void setup()
   
   // Reset the trigger pin and wait a half a second
   digitalWrite(TRIGGER_PIN_x, LOW);
-//++++++++++++++++++++++++++++++++++++++++++++++++++++====
- /* // Initializing Trigger Output and Echo Input
-  pinMode(TRIGGER_PIN_y, OUTPUT);
-  pinMode(ECHO_PIN_y, INPUT);
-  
-  // Reset the trigger pin and wait a half a second
-  digitalWrite(TRIGGER_PIN_y, LOW);
-  */
-//+++++++++++++++++++++++++++++++++++++++++++++++++++++===
+
   delayMicroseconds(500);
 }
 
@@ -66,36 +53,8 @@ long measure_x()
   return measureSum / MEASURE_SAMPLES;
 }
 
-
-/*long singleMeasurement_y()
-{
-  long duration = 0;
-  
-  // Measure: Put up Trigger...
-  digitalWrite(TRIGGER_PIN_y, HIGH);
-  // ... wait for 11 µs ...
-  delayMicroseconds(11);
-  // ... put the trigger down ...
-  digitalWrite(TRIGGER_PIN_y, LOW);
-  // ... and wait for the echo ...
-  duration = pulseIn(ECHO_PIN_y, HIGH);
-  return (long) (((float) duration / USONIC_DIV) * 10.0);
-}
-
-long measure_y()
-{
-  long measureSum_ = 0;
-  for (int i = 0; i < MEASURE_SAMPLES; i++)
-  {
-    delay(MEASURE_SAMPLE_DELAY);
-    measureSum_ += singleMeasurement_y();
-  }
-  return measureSum_ / MEASURE_SAMPLES;
-}
-*/
 void loop()
 {
-  //+++++++++++++++++++++++++++++++++++++++++Lectura ultrasonico en eje X+++++++++++++++++++++++++
   delay(MEASURE_DELAY);
   int distance_x = measure_x()*0.1;
   
@@ -103,14 +62,5 @@ void loop()
   //Serial.println(x_);
  analogWrite(PWM_PIN,distance_x);
   Serial.println(distance_x);
- 
-  //+++++++++++++++++++++++++++++++++++++++++Lectura ultrasonico en eje Y+++++++++++++++++++++++++
-/*  delay(MEASURE_DELAY);
-  int distance_y = measure_y();
 
-  sprintf(y_, "%dy", distance_y);
-  Serial.println(y_);
-
-  Serial.print(distance_y);
- */ 
 }
